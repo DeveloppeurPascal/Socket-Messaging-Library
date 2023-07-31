57e2cbeb371a9a4566d7e4e7a8ed6950dac13145
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-07-31 19:24:59 +0200

    added uProject unit

--- src-generator/SocketMessagesGenerator.dpr
+++ src-generator/SocketMessagesGenerator.dpr
@@ -6,7 +6,8 @@ uses
   fMain in 'fMain.pas' {Form1},
   Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
   Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
-  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas';
+  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas',
+  uProject in 'uProject.pas';
 
 {$R *.res}
 
