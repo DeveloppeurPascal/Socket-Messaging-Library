a19c9dd00e37c567be679be5749c6d92e4767128
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-08-01 18:04:09 +0200

    added the Socket Messaging library unit to the project

--- src-generator/SocketMessagesGenerator.dpr
+++ src-generator/SocketMessagesGenerator.dpr
@@ -7,7 +7,8 @@ uses
   Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
   Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
   u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas',
-  uProject in 'uProject.pas';
+  uProject in 'uProject.pas',
+  Olf.Net.Socket.Messaging in '..\src-library\Olf.Net.Socket.Messaging.pas';
 
 {$R *.res}
 
