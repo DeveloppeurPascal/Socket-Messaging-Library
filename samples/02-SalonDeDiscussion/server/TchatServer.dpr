f02f905df505e49cf1eda616caa508b04522eb07
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-03 19:33:56 +0200

    updated the sample to "libraries" library changes

--- samples/02-SalonDeDiscussion/server/TchatServer.dpr
+++ samples/02-SalonDeDiscussion/server/TchatServer.dpr
@@ -5,8 +5,9 @@ uses
   FMX.Forms,
   fServerForm in 'fServerForm.pas' {Form2},
   SalonDeDiscussion in '..\SalonDeDiscussion.pas',
-  Olf.RTL.Streams in '..\..\..\lib-externes\librairies\Olf.RTL.Streams.pas',
-  Olf.Net.Socket.Messaging in '..\..\..\src\Olf.Net.Socket.Messaging.pas';
+  Olf.Net.Socket.Messaging in '..\..\..\src\Olf.Net.Socket.Messaging.pas',
+  Olf.RTL.Maths.Conversions in '..\..\..\lib-externes\librairies\src\Olf.RTL.Maths.Conversions.pas',
+  Olf.RTL.Streams in '..\..\..\lib-externes\librairies\src\Olf.RTL.Streams.pas';
 
 {$R *.res}
 
